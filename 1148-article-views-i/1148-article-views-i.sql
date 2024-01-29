74e773c2f34d35361de792a88784e87f7e81439f
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-30 00:18:42 +0530

    Time: 707 ms (71.38%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,7 @@
--- Select distinct 'author_id' from 'Views' where 'author_id' is equal to 'viewer_id'
+-- Select distinct 'author_id' from the 'Views' table 
+-- where 'author_id' is equal to 'viewer_id' (indicating that the author viewed their own article)
 SELECT DISTINCT author_id AS id
 FROM Views
-WHERE author_id = viewer_id ORDER BY author_id;
+WHERE author_id = viewer_id
+-- Order the result by 'author_id' in ascending order
+ORDER BY author_id;
