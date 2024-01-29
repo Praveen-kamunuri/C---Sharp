1d5e14607fe904e8705dc4dcb80a44cf8cdb9525
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-30 00:03:06 +0530

    Time: 1134 ms (19.23%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-SELECT name FROM Customer  WHERE referee_id IS NULL OR referee_id != '2';
+-- Select the 'name' column from the 'Customer' table
+SELECT name 
+-- Filter rows where 'referee_id' is either NULL (not referred by anyone) or not equal to '2' (referred by someone other than customer with id = 2)
+FROM Customer 
+WHERE referee_id IS NULL OR referee_id != '2';
