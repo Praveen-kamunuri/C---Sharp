4342502f1c41bb5c59be0fae63539d77586e574b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-30 00:09:14 +0530

    Time: 800 ms (10.59%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-
-SELECT name , population , area FROM WORLD WHERE area >= 3000000 OR population >= 25000000;
+-- Select the 'name', 'population', and 'area' columns from the 'World' table
+SELECT name, population, area
+-- Filter countries that have an area of at least 3 million or a population of at least 25 million
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
