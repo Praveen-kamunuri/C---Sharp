811c188e4982e7d79a9e0e3608737c1a7b18b41c
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:26:53 +0530

    Delete 1378-replace-employee-id-with-the-unique-identifier directory

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++
@@ -1,6 +0,0 @@
--- Select the unique_id from EmployeeUNI and the name from Employees
-SELECT euni.unique_id, e.name
--- From the Employees table (aliased as 'e') 
-FROM Employees e
--- Left join with the EmployeeUNI table (aliased as 'euni') based on employee IDs
-LEFT JOIN EmployeeUNI euni ON e.id = euni.id;
