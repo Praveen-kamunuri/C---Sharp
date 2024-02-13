01f3740d841221fefca95bcc107edce64c301fd0
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:26:25 +0530

    Delete 0584-find-customer-referee directory

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++
@@ -1,5 +0,0 @@
--- Select the 'name' column from the 'Customer' table
-SELECT name 
--- Filter rows where 'referee_id' is either NULL (not referred by anyone) or not equal to '2' (referred by someone other than customer with id = 2)
-FROM Customer 
-WHERE referee_id IS NULL OR referee_id != '2';
