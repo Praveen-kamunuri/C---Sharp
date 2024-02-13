fbcf9d4b3092287ac86c89d597df734f99cb80b8
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:36:09 +0530

    Time: 25 ms (44.61%), Space: 26.9 MB (63.45%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        long ans = 0;
+        while (x != 0)
+        {
+            int rem = x % 10;
+            ans = ans * 10 + rem;
+            x = x / 10;
+        }
+        
+        if (ans < int.MinValue || ans > int.MaxValue)
+        {
+            return 0; // Return 0 if result overflows
+        }
+        
+        return (int)ans;
+    }
+}
