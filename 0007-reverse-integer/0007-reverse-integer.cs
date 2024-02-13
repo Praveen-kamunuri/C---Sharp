eb0d1f0fdd8ae825298a9373b9de2c41dfd8da5b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:45:03 +0530

    Time: 25 ms (44.61%), Space: 26.9 MB (56.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cs
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -3,18 +3,18 @@ public class Solution
     public int Reverse(int x)
     {
         long ans = 0;
-        while (x != 0)
+        
+        while( x != 0)
         {
             int rem = x % 10;
             ans = ans * 10 + rem;
             x = x / 10;
         }
         
-        if (ans < int.MinValue || ans > int.MaxValue)
+        if (ans < int.MinValue || ans > int.MaxValue) 
         {
-            return 0; // Return 0 if result overflows
+            return 0;
         }
-        
         return (int)ans;
     }
 }
