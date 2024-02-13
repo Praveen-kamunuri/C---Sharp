db83e1814e572329a2424ea905a802352a826cdc
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:27:10 +0530

    Delete 1757-recyclable-and-low-fat-products directory

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-
--- Select the product_id from the Products table
-SELECT product_id 
--- Filter rows where low_fats column is 'Y' and recyclable column is 'Y'
-FROM Products 
-WHERE low_fats = 'Y' AND recyclable = 'Y';
