29dc8f62235f0350184481e004901837410a6f05
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-13 22:26:43 +0530

    Delete 1148-article-views-i directory

--- 1148-article-views-i/1148-article-views-i.sql
+++
@@ -1,7 +0,0 @@
--- Select distinct 'author_id' from the 'Views' table 
--- where 'author_id' is equal to 'viewer_id' (indicating that the author viewed their own article)
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
--- Order the result by 'author_id' in ascending order
-ORDER BY author_id;
