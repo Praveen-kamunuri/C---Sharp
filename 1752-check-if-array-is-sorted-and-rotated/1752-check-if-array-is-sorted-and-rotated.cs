745383e42662215f554a6ab6ee8d69bef064a9da
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-16 00:09:21 +0530

    Time: 64 ms (79.27%), Space: 42.6 MB (7.32%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
@@ -2,26 +2,28 @@ public class Solution
 {
     public bool Check(int[] nums)
     {
-        int count = 0;
+        int count = 0;  // Counter for the number of times the array is rotated
         
-        int n = nums.Length;
+        int n = nums.Length;  // Get the length of the array
         
+        // Iterate through the array to check if it's rotated
         for(int i = 1; i < n; i++)
         {
-            if (nums[i-1] > nums[i])
+            if (nums[i-1] > nums[i])  // If current element is smaller than previous, it indicates rotation
             {
-                count++;
+                count++;  // Increment the counter
                 
             }
             
-            
         }
         
-        if (nums[n-1] > nums[0])
+        // Check the transition between the last and first elements
+        if (nums[n-1] > nums[0])  // If last element is greater than first, it indicates rotation
         {
-            count++;
+            count++;  // Increment the counter
         }
         
+        // If the counter is less than or equal to 1, it means the array is sorted and rotated
         return count <= 1;
     }
 }
